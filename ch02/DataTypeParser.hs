c661de309398ca9e06f6cadb9b42750afbbba856
David Beacham <david.beacham@geneity.co.uk>
2012-02-02 13:35:50 +0000

    Add first 3 exerices of chapter two to DataTypeParser

--- ch02/DataTypeParser.hs
+++ ch02/DataTypeParser.hs
@@ -9,8 +9,26 @@ main = do
   (arg:_) <- getArgs
   putStrLn $ readExpr $ arg
 
+readExpr :: String -> String
+readExpr input =
+  case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value: " ++ show val
+
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+-- Ex2,3. R5Rs allows for internally escaped quotes
+escapedChars :: Parser Char
+escapedChars = do
+  char '\\'
+  x <- oneOf "\\\"nrt"
+  return $ case x of
+    '\\' -> x
+    '"' -> x
+    'n' -> '\n'
+    'r' -> '\r'
+    't' -> '\t'
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -21,11 +39,15 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+  deriving Show
 
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  x <- many (noneOf "\"")
+  -- non-R5RS compliant
+  -- x <- many (noneOf "\"")
+  -- Ex2,3. R5Rs allows for internally escaped quotes and special characters
+  x <- many $ escapedChars <|> noneOf "\""
   char '"'
   return $ String x
 
@@ -40,17 +62,18 @@ parseAtom = do
              otherwise -> Atom atom
 
 parseNumber :: Parser LispVal
+-- liftM version
 parseNumber = liftM (Number . read) $ many1 digit
+
+-- Ex1a. do-notation version
+-- parseNumber = do
+--  nbrStr <- many1 digit
+--  return $ Number $ read nbrStr
+
+-- Ex1b. explicit monad form
+-- parseNumber = many1 digit >>= \s -> return $ Number $ read s
 
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
         <|> parseNumber
-
-readExpr :: String -> String
-readExpr input =
-  case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
-
-
